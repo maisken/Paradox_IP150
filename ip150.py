ca3e3feeaa49d7d1b68136cce756852b3ad8f5a9
Alfredo Pironti <alfredo@pironti.eu>
2018-01-02 22:30:12 +0100

    Better parsing of alarm data Ability to subscribe to alarm status updates Better handling of timing intervals Introduced map for controlling the alarm

--- ip150.py
+++ ip150.py
@@ -5,15 +5,16 @@ import threading
 from bs4 import BeautifulSoup
 import re
 import json
+import functools
 
 
-class Paradox_IP150_Login(Exception):
+class Paradox_IP150_Error(Exception):
     pass
 
 
 class KeepAlive(threading.Thread):
 
-    def __init__(self, ip150url, interval=5.0):
+    def __init__(self, ip150url, interval):
         threading.Thread.__init__(self, daemon=True)
         self.ip150url = ip150url
         self.interval = interval
@@ -21,7 +22,7 @@ class KeepAlive(threading.Thread):
 
     def _one_keepalive(self):
         requests.get('{}/keep_alive.html'.format(
-            self.ip150url), params={'msgid': 1})
+            self.ip150url), params={'msgid': 1}, verify=False)
 
     def run(self):
         while not self.stopped.wait(self.interval):
@@ -33,16 +34,58 @@ class KeepAlive(threading.Thread):
 
 class Paradox_IP150:
 
-    _varmap = {
-        'alarms': 'tbl_alarmes',
-        'troubles': 'tbl_troubles',
-        'status_zone': 'tbl_statuszone'
+    _tables_map = {
+        # A map from human readable info about the alarm, to "table" (in fact, array) names used in IP150 software
+        #'triggered_alarms': 'tbl_alarmes', # Redundant list of zones with an alarm currently triggered. A zone in alarm will also be reported in the 'tbl_useraccess' table
+        #'troubles': 'tbl_troubles', # Could use this list to publish alarm troubles, not required for now
+        # Could use the next list to publish zone status (0=Closed, 1=Open), not required for now
+        #'zone_status': {
+        #    'name': 'tbl_statuszone',
+        #    'map' : {
+        #        0: 'Closed',
+        #        1: 'Open'
+        #    }
+        #}
+        # The next list provides the status (as an integer, 0 for area not enabled) for each supported area
+        'areas_status': {
+            'name': 'tbl_useraccess',
+            'map' : {
+                0: 'Unset',
+                1: 'Disarmed',
+                2: 'Armed',
+                3: 'Triggered',
+                4: 'Armed_sleep',
+                5: 'Armed_stay',
+                6: 'Entry_delay',
+                7: 'Exit_delay'
+            }
+        }
+    }
+
+    _areas_action_map = {
+        # Mappring from human readable commands to machine readable
+        'Disarm'   : 'd',
+        'Arm'      : 'r',
+        'Arm_sleep': 'p',
+        'Arm_stay' : 's'
     }
 
     def __init__(self, addr, port=80, method='http'):
         self.ip150url = '{}://{}:{}'.format(method, addr, port)
         self.logged_in = False
         self._keepalive = None
+        self._updates = None
+        self._stop_updates = threading.Event()
+
+    def _logged_only(f):
+        @functools.wraps(f)
+        def wrapped(self, *args, **kwargs):
+            if not self.logged_in:
+                raise Paradox_IP150_Error(
+                    'Not logged in; please use login() first.')
+            else:
+                return f(self, *args, **kwargs)
+        return wrapped
 
     def _to_8bits(self, s):
         return "".join(map(lambda x: chr(ord(x) % 256), s))
@@ -73,9 +116,9 @@ class Paradox_IP150:
         return {'p': hashlib.md5(spass.encode('ascii')).hexdigest().upper(),
                 'u': self._paradox_rc4(user, spass)}
 
-    def login(self, user, pwd, keep_alive=True):
+    def login(self, user, pwd, keep_alive_interval=5.0):
         if self.logged_in:
-            raise Paradox_IP150_Login(
+            raise Paradox_IP150_Error(
                 'Already logged in; please use logout() first.')
 
         # Ask for a login page, to get the 'sess' salt
@@ -85,7 +128,7 @@ class Paradox_IP150:
         # Extract the 'sess' salt
         off = lpage.text.find('loginaff')
         if off == -1:
-            raise Paradox_IP150_Login(
+            raise Paradox_IP150_Error(
                 'Wrong page fetcehd. Did you connect to the right server and port? Server returned: {}'.format(lpage.text))
         sess = lpage.text[off + 10:off + 26]
 
@@ -95,27 +138,30 @@ class Paradox_IP150:
             self.ip150url), params=creds, verify=False)
         if defpage.text.count("top.location.href='login_page.html';") > 0:
             # They're redirecting us to the login page; credentials didn't work
-            raise Paradox_IP150_Login(
+            raise Paradox_IP150_Error(
                 'Could not login, wrong credentials provided.')
         # Give enough time to the server to set up.
         time.sleep(3)
-        if keep_alive:
-            self._keepalive = KeepAlive(self.ip150url)
+        if keep_alive_interval:
+            self._keepalive = KeepAlive(self.ip150url, keep_alive_interval)
             self._keepalive.start()
         self.logged_in = True
 
+    @_logged_only
     def logout(self):
-        if not self.logged_in:
-            raise Paradox_IP150_Login(
-                'Not logged in; please use login() first.')
-
         if self._keepalive:
             self._keepalive.cancel()
+            self._keepalive.join()
             self._keepalive = None
+        if self._updates:
+            self._stop_updates.set()
+            self._updates.join()
+            self._stop_updates.clear()
+            self._updates = None
         logout = requests.get(
             '{}/logout.html'.format(self.ip150url), verify=False)
         if logout.status_code != 200:
-            raise Paradox_IP150_Login('Error logging out')
+            raise Paradox_IP150_Error('Error logging out')
         self.logged_in = False
 
     def _js2array(self, varname, script):
@@ -123,14 +169,59 @@ class Paradox_IP150:
         res = '[{}]'.format(res.group(1))
         return json.loads(res)
 
+    @_logged_only
     def get_info(self):
         status_page = requests.get(
             '{}/statuslive.html'.format(self.ip150url), verify=False)
         status_parsed = BeautifulSoup(status_page.text, 'html.parser')
         if status_parsed.find('form', attrs={'name': 'statuslive'}) is None:
-            raise Paradox_IP150_Login('Could not retrieve status information')
+            raise Paradox_IP150_Error('Could not retrieve status information')
         script = status_parsed.find('script').string
         res = {}
-        for varname in self._varmap.keys():
-            res[varname] = self._js2array(self._varmap[varname], script)
+        for table in self._tables_map.keys():
+            #Extract the js array for the current "table"
+            tmp = self._js2array(self._tables_map[table]['name'], script)
+            #Map the extracted machine values to the corresponding human values
+            res[table] = [(i, self._tables_map[table]['map'][x]) for i,x in enumerate(tmp, start=1)]
         return res
+
+    def _get_updates(self, on_update, interval):
+        prev_state = {}
+
+        while not self._stop_updates.wait(interval):
+            updated_state = {}
+            cur_state = self.get_info()
+            for d1 in cur_state.keys():
+                if d1 in prev_state:
+                    for cur_d2, prev_d2 in zip(cur_state[d1], prev_state[d1]):
+                        if cur_d2 != prev_d2:
+                            if d1 in updated_state:
+                                updated_state[d1].append(cur_d2)
+                            else:
+                                updated_state[d1] = [cur_d2]
+                else:
+                    updated_state[d1] = cur_state[d1]
+
+            if len(updated_state) > 0:
+                on_update(updated_state)
+
+            prev_state = cur_state
+
+    @_logged_only
+    def get_updates(self, on_update=None, poll_interval=1.0):
+        if not on_update:
+            raise Paradox_IP150_Error('The callable on_update must be provided.')
+        if poll_interval <= 0.0:
+            raise Paradox_IP150_Error('The polling interval must be greater than 0.0 seconds.')
+        self._updates = threading.Thread(target=self._get_updates, args=(on_update, poll_interval), daemon=True)
+        self._updates.start()
+
+    @_logged_only
+    def cancel_updates(self):
+        if self._updates:
+            self._stop_updates.set()
+            self._updates.join()
+            self._stop_updates.clear()
+            self._updates = None
+        else:
+            raise Paradox_IP150_Error('Not currently getting updates. Use get_updates() first.')
